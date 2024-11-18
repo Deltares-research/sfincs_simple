3a81e71a9f4a7c938995bb9589fb53a37f221eb6
Leynse <Tim.Leijnse@deltares.nl>
2024-11-18 16:42:09 +0100

    - Build GPU exe on main slurm node after adding 'module load nvidia' - Seems to build correctly, however not been able to test running exe because of inactive GPU node - Increase size domain for better performance testing

--- build_nvfortran_gpu.sh
+++ build_nvfortran_gpu.sh
@@ -2,10 +2,12 @@
 MANPATH=$MANPATH:/opt/nvidia/hpc_sdk/Linux_x86_64/24.5/compilers/man; export MANPATH
 PATH=/opt/nvidia/hpc_sdk/Linux_x86_64/24.5/compilers/bin:$PATH; export PATH
 
+module load nvidia
+
 #nvfortran -acc=gpu -Minfo=accel -fast -O3 -gpu=ccall sfincs_unstructured/sfincs_unstructured.f90 -o unstructured.exe
-#nvfortran -acc=gpu -Minfo=accel -fast -O3 -gpu=ccall sfincs_structured/sfincs_structured.f90 -o structured.exe
+nvfortran -acc=gpu -Minfo=accel -fast -O3 -gpu=ccall sfincs_structured/sfincs_structured.f90 -o structured.exe
 
 # input arguments: nmax, mmax, nt, copy, momentum, continuity
 
-./unstructured.exe 1000 1000 1000 T T T
-./structured.exe 1000 1000 1000 T T T
+#./unstructured.exe 4096 4096 1000 T T T
+./structured.exe 4096 4096 1000 T T T
